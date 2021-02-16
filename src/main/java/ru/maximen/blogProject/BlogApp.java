f53cf8b32bd48ea8b10de201508224d4463b431f
Sergei Maksimenko <serj.maximenko@gmail.com>
2021-02-16 18:00:04 +0300

    start

---
+++ src/main/java/ru/maximen/blogProject/BlogApp.java
@@ -0,0 +1,9 @@
+package ru.maximen.blogProject;
+
+import org.springframework.boot.SpringApplication;
+
+public class BlogApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApp.class, args);
+    }
+}
